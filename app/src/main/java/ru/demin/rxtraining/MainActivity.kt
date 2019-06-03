33b8523c029cedb6a5d30ac3967baa104422dfcf
Aleksey Demin <a.demin@redmadrobot.com>
2019-06-03 17:49:19 +0300

    [develop] retryWhen operator Training

--- app/src/main/java/ru/demin/rxtraining/MainActivity.kt
+++ app/src/main/java/ru/demin/rxtraining/MainActivity.kt
@@ -3,12 +3,13 @@ package ru.demin.rxtraining
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import ru.demin.rxtraining.TryConcatWith.tryConcatWithError
+import ru.demin.rxtraining.TryRetry.tryRetry
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tryConcatWithError()
+        tryRetry()
     }
 }
---
+++ app/src/main/java/ru/demin/rxtraining/TryRetry.kt
@@ -0,0 +1,37 @@
+package ru.demin.rxtraining
+
+import android.annotation.SuppressLint
+import io.reactivex.Observable
+import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
+import java.util.concurrent.TimeUnit
+import kotlin.math.log
+import kotlin.text.Typography.times
+
+object TryRetry {
+    private var emitCount = 0
+
+    /*Observable который выдаёт либо ошибку, если подписывались меньше 5 раз, либо число*/
+    private val observable = Observable.create<Int> { emitter ->
+        emitCount++
+        log("emit $emitCount")
+        if (emitCount < 5) emitter.onError(RuntimeException("test"))
+        else (emitter.onNext(3))
+    }
+
+    /*retry с задержкой*/
+    @SuppressLint("CheckResult")
+    fun tryRetry() {
+        log("Start")
+        observable
+            .retryWhen { it.delay(1, TimeUnit.SECONDS) }
+            .subscribe({
+                log("Success $it")
+            }, {
+                log("Error")
+            },{
+                log("onComplete")
+            })
+    }
+
+}
