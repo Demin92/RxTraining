5ca3535710bda1fbe30884bae5607d321d839df9
Aleksei Demin <a.demin@prequelapp.com>
2021-06-18 15:38:34 +0300

    add error check

--- app/src/main/java/ru/demin/rxtraining/ListOfSingleToSingleOfList.kt
+++ app/src/main/java/ru/demin/rxtraining/ListOfSingleToSingleOfList.kt
@@ -2,14 +2,21 @@ package ru.demin.rxtraining
 
 import android.annotation.SuppressLint
 import io.reactivex.Single
+import java.io.IOException
 
 object ListOfSingleToSingleOfList {
 
     @SuppressLint("CheckResult")
     fun tryList() {
-        val singles = List(5) { index -> index }.map { Single.create<Int> { emitter -> emitter.onSuccess(it) } }
+        val singles = List(5) { index -> index }.map {
+            Single.create<Int> { emitter ->
+                if (it == 3) emitter.onError(IOException()) else emitter.onSuccess(it)
+            }
+        }
         Single.concat(singles).toList().subscribe({
             log("success result is $it")
-        }, {})
+        }, {
+            log("error $it")
+        })
     }
 }
